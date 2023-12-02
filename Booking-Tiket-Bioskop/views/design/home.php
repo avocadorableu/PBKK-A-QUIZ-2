7994926675f8d8f5a13a55f6766f811e94a97ce0
avocadorableu <119839071+avocadorableu@users.noreply.github.com>
2023-12-02 14:10:24 +0800

    Add files via upload

---
+++ Booking-Tiket-Bioskop/views/design/home.php
@@ -0,0 +1,10 @@
+<div class="page-content">
+<div class="page-body">
+                    <div class="row">
+                        <h4>Aplikasi ini belum selesai semua baru sampe view kursi</h4>
+<h4>Kalo mau liat yang laen bisa klik <a href="https://educatecode.com"><strong>disini</strong></a> atau <a href="https://www.facebook.com/tutorialpemograman/posts/1828760290779916"><strong>disini</strong></a> </h4>						
+                        </div>
+
+                    </div>
+                </div>
+			
